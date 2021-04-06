0b98560c9914fc57a98540915fffe4e71fb18e32
bin6246 <bin6246@naver.com>
2021-04-06 15:01:14 +0900

    듣보잡 푸는중

--- Study/Week_8/1764_듣보잡/BOJ_1764_듣보잡_yunbin.py
+++ Study/Week_8/1764_듣보잡/BOJ_1764_듣보잡_yunbin.py
@@ -1,8 +1,21 @@
 import sys
-
 sys.stdin = input('input.txt')
 
 N, M = map(int,input().split())
 
-N_name = {}
-M_name ={}
+N_name = []
+M_name =[]
+
+for _ in range(N):
+    n_name = input()
+    N_name.append(n_name)
+for _ in range(M):
+    m_name = input()
+    M_name.append(m_name)
+
+result =[]
+for i in range(N):
+    if N_name[i] not in M_name:
+        result.append(N_name)
+
+print(result)
