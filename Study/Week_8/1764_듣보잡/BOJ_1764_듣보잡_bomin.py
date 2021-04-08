f127776f9a6f63a75aaba8f8ee5db8ebaf06f414
bomin1 <rlaqhals0134@naver.com>
2021-04-08 17:13:33 +0900

    Update 다른 풀이가 들어가 있었음

--- Study/Week_8/1764_듣보잡/BOJ_1764_듣보잡_bomin.py
+++ Study/Week_8/1764_듣보잡/BOJ_1764_듣보잡_bomin.py
@@ -1,31 +1,18 @@
 import sys
 sys.stdin = open("input.txt")
-#
-# n,m = list(map(int, input().split()))
-#
-# a = set()
-# b = set()
-# for i in range(n):
-#     a.add(input())
-# for i in range(m):
-#     b.add(input())
-#
-# res = sorted(list(a&b))
-# print(len(res))
-# for val in res:
-#     print(val)
-
 
 n,m = list(map(int, input().split()))
 
-a = []
-b = []
+a = set()
+b = set()
 for i in range(n):
-    a.append(input())
+    a.add(input())
 for i in range(m):
-    b.append(input())
+    b.add(input())
 
 res = sorted(list(a&b))
 print(len(res))
 for val in res:
     print(val)
+
+
